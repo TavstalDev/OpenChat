731e71ef7ee9ccecf466c1811ad4fee1070068b2
tavstal <tavstal@gmail.com>
2025-09-09 16:43:17 +0200

    Add CommandChat class with subcommands for help, version, reload, and clear

---
+++ src/main/java/io/github/tavstaldev/openChat/commands/CommandChat.java
@@ -0,0 +1,197 @@
+package io.github.tavstaldev.openChat.commands;
+
+import io.github.tavstaldev.minecorelib.core.PluginLogger;
+import io.github.tavstaldev.minecorelib.models.command.SubCommandData;
+import io.github.tavstaldev.minecorelib.utils.ChatUtils;
+import io.github.tavstaldev.openChat.OpenChat;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CommandChat implements CommandExecutor {
+    private final PluginLogger _logger = OpenChat.Logger().WithModule(CommandChat.class);
+    private final List<SubCommandData> _subCommands = new ArrayList<>() {
+        {
+            // HELP
+            add(new SubCommandData("help", "openchat.commands.help", Map.of(
+                "syntax", "",
+                "description", "Commands.Help.Desc"
+            )));
+            // VERSION
+            add(new SubCommandData("version", "openchat.commands.version", Map.of(
+                "syntax", "",
+                "description", "Commands.Version.Desc"
+            )));
+            // RELOAD
+            add(new SubCommandData("reload", "openchat.commands.reload", Map.of(
+               "syntax", "",
+               "description", "Commands.Reload.Desc"
+            )));
+            // CLEAR
+            add(new SubCommandData("clear", "openchat.commands.clear", Map.of(
+                "syntax", "",
+                "description", "Commands.Clear.Desc"
+            )));
+        }
+    };
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String @NotNull [] args) {
+        if (sender instanceof ConsoleCommandSender) {
+            _logger.Info(ChatUtils.translateColors("Commands.ConsoleCaller", true).toString());
+            return true;
+        }
+        Player player = (Player) sender;
+        if (args.length > 0) {
+            switch (args[0].toLowerCase()) {
+                case "help":
+                case "?": {
+                    if (!player.hasPermission("openchat.commands.help")) {
+                        OpenChat.Instance.sendLocalizedMsg(player, "General.NoPermission");
+                        return true;
+                    }
+
+                    int page = 1;
+                    if (args.length > 1) {
+                        try {
+                            page = Integer.parseInt(args[1]);
+                        } catch (Exception ex) {
+                            OpenChat.Instance.sendLocalizedMsg(player, "Commands.Common.InvalidPage");
+                            return true;
+                        }
+                    }
+
+                    help(player, page);
+                    return true;
+                }
+                case "version": {
+                    if (!player.hasPermission("openchat.commands.version")) {
+                        OpenChat.Instance.sendLocalizedMsg(player, "General.NoPermission");
+                        return true;
+                    }
+
+                    Map<String, Object> parameters = new HashMap<>();
+                    parameters.put("version", OpenChat.Instance.getVersion());
+                    OpenChat.Instance.sendLocalizedMsg(player, "Commands.Version.Current", parameters);
+
+                    OpenChat.Instance.isUpToDate().thenAccept(upToDate -> {
+                        if (upToDate) {
+                            OpenChat.Instance.sendLocalizedMsg(player, "Commands.Version.UpToDate");
+                        } else {
+                            OpenChat.Instance.sendLocalizedMsg(player, "Commands.Version.Outdated", Map.of("link", OpenChat.Instance.getDownloadUrl()));
+                        }
+                    }).exceptionally(e -> {
+                        _logger.Error("Failed to determine update status: " + e.getMessage());
+                        return null;
+                    });
+                    return true;
+                }
+                case "reload": {
+                    if (!player.hasPermission("openchat.commands.reload")) {
+                        OpenChat.Instance.sendLocalizedMsg(player, "General.NoPermission");
+                        return true;
+                    }
+
+                    OpenChat.Instance.reload();
+                    OpenChat.Instance.sendLocalizedMsg(player, "Commands.Reload.Done");
+                    return true;
+                }
+                case "clear": {
+                    if (!player.hasPermission("openchat.commands.clear")) {
+                        OpenChat.Instance.sendLocalizedMsg(player, "General.NoPermission");
+                        return true;
+                    }
+
+                    for (var onlinePlayer : OpenChat.Instance.getServer().getOnlinePlayers()) {
+                        if (onlinePlayer.hasPermission("openchat.bypass.clear")) continue;
+                        for (int i = 0; i < 300; i++) {
+                            onlinePlayer.sendMessage(Component.text("\n\n\n"));
+                        }
+                        OpenChat.Instance.sendLocalizedMsg(onlinePlayer, "Commands.Clear.Done", Map.of("player", player.getName()));
+                    }
+
+                    OpenChat.Instance.sendLocalizedMsg(player, "Commands.Clear.Done", Map.of("player", player.getName()));
+                    return  true;
+                }
+            }
+
+            OpenChat.Instance.sendLocalizedMsg(player, "Commands.InvalidArguments");
+            return true;
+        }
+
+        if (!player.hasPermission("openchat.commands.help")) {
+            OpenChat.Instance.sendLocalizedMsg(player, "General.NoPermission");
+            return true;
+        }
+        help(player, 1);
+        return true;
+    }
+
+    private void help(Player player, int page) {
+        int maxPage = 1 + (_subCommands.size() / 15);
+
+        if (page > maxPage)
+            page = maxPage;
+        if (page < 1)
+            page = 1;
+        int finalPage = page;
+
+        OpenChat.Instance.sendLocalizedMsg(player, "Commands.Help.Title", Map.of(
+                        "current_page", finalPage,
+                        "max_page", maxPage
+                )
+        );
+        OpenChat.Instance.sendLocalizedMsg(player, "Commands.Help.Info");
+
+        boolean reachedEnd = false;
+        int itemIndex = 0;
+        for (int i = 0; i < 15; i++) {
+            int index = itemIndex + (page - 1) * 15;
+            if (index >= _subCommands.size()) {
+                reachedEnd = true;
+                break;
+            }
+            itemIndex++;
+
+            SubCommandData subCommand = _subCommands.get(index);
+            if (!subCommand.hasPermission(player)) {
+                i--;
+                continue;
+            }
+
+            subCommand.send(OpenChat.Instance, player);
+        }
+
+        // Bottom message
+        String previousBtn = OpenChat.Instance.Localize(player, "Commands.Help.PrevBtn");
+        String nextBtn = OpenChat.Instance.Localize(player, "Commands.Help.NextBtn");
+        String bottomMsg = OpenChat.Instance.Localize(player, "Commands.Help.Bottom")
+                .replace("%current_page%", String.valueOf(page))
+                .replace("%max_page%", String.valueOf(maxPage));
+
+        Map<String, Component> bottomParams = new HashMap<>();
+        if (page > 1)
+            bottomParams.put("previous_btn", ChatUtils.translateColors(previousBtn, true).clickEvent(ClickEvent.runCommand("/openchat help " + (page - 1))));
+        else
+            bottomParams.put("previous_btn", ChatUtils.translateColors(previousBtn, true));
+
+        if (!reachedEnd && maxPage >= page + 1)
+            bottomParams.put("next_btn", ChatUtils.translateColors(nextBtn, true).clickEvent(ClickEvent.runCommand("/openchat help " + (page + 1))));
+        else
+            bottomParams.put("next_btn", ChatUtils.translateColors(nextBtn, true));
+
+        Component bottomComp = ChatUtils.buildWithButtons(bottomMsg, bottomParams);
+        player.sendMessage(bottomComp);
+    }
+}
