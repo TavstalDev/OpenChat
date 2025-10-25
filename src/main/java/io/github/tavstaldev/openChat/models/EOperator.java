e0a3bd5964be6be04cf29b075e05e1539b7158a9
tavstal <tavstal@gmail.com>
2025-10-25 17:02:34 +0200

    feat: add EOperator enum for comparison operators in condition evaluation

---
+++ src/main/java/io/github/tavstaldev/openChat/models/EOperator.java
@@ -0,0 +1,25 @@
+package io.github.tavstaldev.openChat.models;
+
+/**
+ * The EOperator enum represents various comparison operators
+ * that can be used to evaluate conditions.
+ */
+public enum EOperator {
+    /** Represents the equality operator (==). */
+    EQUALS,
+
+    /** Represents the not-equal operator (!=). */
+    NOT_EQUALS,
+
+    /** Represents the greater-than operator (>). */
+    GREATER_THAN,
+
+    /** Represents the less-than operator (<). */
+    LESS_THAN,
+
+    /** Represents the greater-than-or-equal operator (>=). */
+    GREATER_THAN_OR_EQUAL,
+
+    /** Represents the less-than-or-equal operator (<=). */
+    LESS_THAN_OR_EQUAL
+}
