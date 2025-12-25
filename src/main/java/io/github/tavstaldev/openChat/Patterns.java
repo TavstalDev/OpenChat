e52eab8076e236a9d0006cdec9751a4b66bc12f2
tavstal <tavstal@gmail.com>
2025-12-25 15:50:45 +0100

    feat: add hex color pattern for color validation in chat messages

--- src/main/java/io/github/tavstaldev/openChat/Patterns.java
+++ src/main/java/io/github/tavstaldev/openChat/Patterns.java
@@ -7,4 +7,5 @@ public class Patterns {
     public static final Pattern legacyPattern = Pattern.compile("(?i)[&§]([0-9a-fk-or])");
     public static final Pattern hexPattern = Pattern.compile("(?i)[&§]#([A-Fa-f0-9]{6})");
     public static final Pattern emojiPattern = Pattern.compile(":[a-zA-Z0-9_]+:");
+    public static final Pattern hexColorPattern = Pattern.compile("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$");
 }
